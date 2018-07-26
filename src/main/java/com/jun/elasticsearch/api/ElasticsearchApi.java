57016fa348431468663d03a9511c1f978847b843
ligohan <www.lijun07@qq.com>
2018-07-26 16:16:49 +0800

    修改默认IP

--- src/main/java/com/jun/elasticsearch/api/ElasticsearchApi.java
+++ src/main/java/com/jun/elasticsearch/api/ElasticsearchApi.java
@@ -2,7 +2,7 @@ package com.jun.elasticsearch.api;
 
 public class ElasticsearchApi {
 
-    public static final String HOST_NAME = "192.168.75.128";
+    public static final String HOST_NAME = "127.0.0.1";
     public static final int PORT = 9200;
     public static final String SCHEME = "http";
 
